60f9e4a8fb1b17bdc0f205b98d2dd6e6fcb7ed2c
Luther Xavier <109442557+lutherxavier@users.noreply.github.com>
2024-06-29 12:51:51 -0300

    Realização exercícios

---
+++ exercicios-praticos/ex008-varios-calculos/script.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector("#btn");
+let res = document.querySelector("#res");
+
+btn.addEventListener("click", function() {
+    let n = prompt("Digite um número:");
+    //valor absoluto
+    //parte inteira
+    //valor inteiro mais próximo
+    //raiz quadrada
+    //raiz cubica
+    //n ao quadrado
+    //n ao cubo
+});
